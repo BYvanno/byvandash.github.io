43a64520420507ce6aa435e93a7088f19acbd293
BYvanno <50427697+BYvanno@users.noreply.github.com>
2023-01-16 18:35:47 +0200

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders=[
+{
+    productName:'Foldable Mini Drone',
+    productNumber:'85631',
+    paymentStatus:'Due',
+    shipping:'Delivered'
+},
+{
+    productName:'Foldable Mini Drone',
+    productNumber:'12331',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName:'Foldable Mini Drone',
+    productNumber:'65461',
+    paymentStatus:'Due',
+    shipping:'PDelivered'
+},
+{
+    productName:'Foldable Mini Drone',
+    productNumber:'85685',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName:'Foldable Mini Drone',
+    productNumber:'54331',
+    paymentStatus:'Due',
+    shipping:'Delivered'
+},
+]
